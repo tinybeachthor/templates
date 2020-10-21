55467950331f35c73bbe80ce9b504508946030c1
Martin Toman <toman.martin@live.com>
2020-10-21 16:27:55 +0200

    Add shell.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
   inputs = {
+    flake-utils.url = github:numtide/flake-utils;
     nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
     tinybeachthor = {
       url = github:tinybeachthor/nur-packages/master;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, tinybeachthor }: {
-    defaultPackage.x86_64-linux = import ./default.nix {
-      pkgs = nixpkgs;
-      inherit tinybeachthor;
-    };
-  };
+  outputs = { self, flake-utils, nixpkgs, tinybeachthor }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs {
+          inherit system;
+          overlays = [ tinybeachthor.overlay ];
+        });
+      in { devShell = import ./shell.nix { inherit pkgs; }; });
 }
